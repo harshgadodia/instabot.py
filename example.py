9f8c96ef3145d06c0819b9f4378dbad6ed36e134
Harsh Gadodia <harsh@u.nus.edu>
2018-08-08 02:17:03 +0800

    create new repo and run for iphonex photography account

--- example.py
+++ example.py
@@ -10,16 +10,18 @@ from src.follow_protocol import follow_protocol
 from src.unfollow_protocol import unfollow_protocol
 
 bot = InstaBot(
-    login="username",
-    password="password",
+    login="harshxiphone",
+    password="2ilovevs234225",
     like_per_day=1000,
     comments_per_day=0,
-    tag_list=['follow4follow', 'f4f', 'cute', 'l:212999109'],
-    tag_blacklist=['rain', 'thunderstorm'],
+    tag_list=['follow4follow', 'f4f', 'travel', 'iphone', 'iphonex', 'iphoneography', 
+    'camera', 'visuals', 'shotoniphone', 'iphonephotography', 'vscocam', 'instaphoto', 'photoftheday', 'visuals',
+     'aesthetics', 'like4like', 'l4l', 'cityscape', 'europe', 'wanderlust', 'traveller'],
+    tag_blacklist=['nsfw'],
     user_blacklist={},
     max_like_for_one_tag=50,
     follow_per_day=300,
-    follow_time=1 * 60,
+    follow_time=1 * 60, 
     unfollow_per_day=300,
     unfollow_break_min=15,
     unfollow_break_max=30,
